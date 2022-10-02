962ffde91d429fefe1adf476c981e8a3791b5ff1
david-leskovar <david.leskovar98@gmail.com>
2022-10-02 13:38:59 +0200

    Added CRUD functionality to API using CQRS+Mediator pattern

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+
+        public class Command : IRequest
+        {
+
+            public Activity Activity { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Command>
+
+
+
+
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                this._context = context;
+                this._mapper = mapper;
+            }
+
+
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                Console.WriteLine(request.Activity.Id);
+
+                var activityToEdit = await _context.Activities.FindAsync(request.Activity.Id);
+
+                _mapper.Map(request.Activity, activityToEdit);
+
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+
+            }
+        }
+
+
+
+
+
+    }
+}
